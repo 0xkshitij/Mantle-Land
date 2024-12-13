590a82b154d9bfcca13109f768124f640fb00aaa
0xkshitij <140337097+0xkshitij@users.noreply.github.com>
2024-12-14 00:03:58 +0530

    Update Forwarder.sol

--- smart_contracts/src/Forwarder.sol
+++ smart_contracts/src/Forwarder.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {ERC2771Forwarder} from "openzeppelin/metatx/ERC2771Forwarder.sol";
+import {ERC2771Forwarder} from "@openzeppelin/contracts/metatx/ERC2771Forwarder.sol";
 
 contract Forwarder is ERC2771Forwarder {
-    constructor() ERC2771Forwarder("Forwarder") {}
+    constructor(string memory _trustedForwarder) ERC2771Forwarder(_trustedForwarder) {}
 }
