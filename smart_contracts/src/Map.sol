26b0cefe68ca1859400b6dfdc59a46a47c63ee1d
0xkshitij <140337097+0xkshitij@users.noreply.github.com>
2024-12-13 23:59:12 +0530

    Update Map.sol

--- smart_contracts/src/Map.sol
+++ smart_contracts/src/Map.sol
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "openzeppelin/token/ERC721/ERC721.sol";
-import "openzeppelin/token/ERC1155/IERC1155.sol";
-import "solmate/utils/LibString.sol";
-
-import {ERC1155TokenReceiver} from "solmate/tokens/ERC1155.sol";
-
-import "openzeppelin/metatx/ERC2771Context.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
 error ZeroSize();
 error ZeroPerSize();
@@ -16,29 +12,22 @@ error InvalidXIndex();
 error InvalidYIndex();
 error LandAlreadyOwned();
 error NotOwner();
-
 error InvalidLength();
 
 contract Map is ERC2771Context, ERC721 {
-    // // rectangular land with coordinates of corners as (x,y), (x,y+perSize), (x+perSize,y), (x+perSize,y+perSize)
     struct Land {
         uint256 xIndex;
         uint256 yIndex;
     }
+
     uint256 public size;
     uint256 public perSize;
     uint256 public landCount;
     string public baseUri;
-    uint256 public utilCount;
     address public utilsAddress;
 
-    // x/perSize,y/perSize to LandId
-    // [ (x/perSize,y/perSize) to LandId ] of rectangular land with coordinates of corners as (x,y), (x,y+perSize), (x+perSize,y), (x+perSize,y+perSize)
     mapping(uint256 => mapping(uint256 => uint256)) public landIds;
-    // mapping of LandId to Land (xIndex, yIndex)
     mapping(uint256 => Land) public land;
-
-    // x,y to utilId
     mapping(uint256 => mapping(uint256 => uint256)) public map;
 
     constructor(
@@ -50,19 +39,19 @@ contract Map is ERC2771Context, ERC721 {
     ) ERC2771Context(trustedForwarder) ERC721("Map", "MAP") {
         if (_size == 0) revert ZeroSize();
         if (_perSize == 0) revert ZeroPerSize();
+        if (_size % _perSize != 0) revert SizeNotDivisibleByPerSize();
+        
         size = _size;
         perSize = _perSize;
-        if (_size % perSize != 0) revert SizeNotDivisibleByPerSize();
         baseUri = _baseUri;
         utilsAddress = _utilsAddress;
     }
 
     function mint(uint256 xIndex, uint256 yIndex) public returns (uint256) {
-        if (xIndex >= size / perSize) revert InvalidXIndex();
-        if (yIndex >= size / perSize) revert InvalidYIndex();
+        if (xIndex >= size / perSize || yIndex >= size / perSize) revert InvalidXIndex();
         if (landIds[xIndex][yIndex] != 0) revert LandAlreadyOwned();
-        landCount += 1;
-        // land[id] = Land(x, y);
+
+        landCount++;
         landIds[xIndex][yIndex] = landCount;
         land[landCount] = Land(xIndex, yIndex);
         _mint(_msgSender(), landCount);
@@ -70,36 +59,30 @@ contract Map is ERC2771Context, ERC721 {
     }
 
     function placeItem(uint256 x, uint256 y, uint256 utilId) public {
-        if (_msgSender() != ownerOf(landIds[x / perSize][y / perSize]))
-            revert NotOwner();
+        uint256 landId = landIds[x / perSize][y / perSize];
+        if (_msgSender() != ownerOf(landId)) revert NotOwner();
+        
         IERC1155 utils = IERC1155(utilsAddress);
-        if (map[x][y] != 0) {
-            utils.safeTransferFrom(
-                address(this),
-                _msgSender(),
-                map[x][y],
-                1,
-                ""
-            );
+        uint256 currentUtilId = map[x][y];
+        
+        if (currentUtilId != 0) {
+            utils.safeTransferFrom(address(this), _msgSender(), currentUtilId, 1, "");
         }
+        
         utils.safeTransferFrom(_msgSender(), address(this), utilId, 1, "");
         map[x][y] = utilId;
     }
 
     function removeItem(uint256 x, uint256 y) public {
-        if (_msgSender() != ownerOf(landIds[x / perSize][y / perSize]))
-            revert NotOwner();
-        IERC1155 utils = IERC1155(utilsAddress);
-        if (map[x][y] != 0) {
-            utils.safeTransferFrom(
-                address(this),
-                _msgSender(),
-                map[x][y],
-                1,
-                ""
-            );
+        uint256 landId = landIds[x / perSize][y / perSize];
+        if (_msgSender() != ownerOf(landId)) revert NotOwner();
+
+        uint256 currentUtilId = map[x][y];
+        if (currentUtilId != 0) {
+            IERC1155 utils = IERC1155(utilsAddress);
+            utils.safeTransferFrom(address(this), _msgSender(), currentUtilId, 1, "");
+            map[x][y] = 0;
         }
-        map[x][y] = 0;
     }
 
     function updateItem(uint256 x, uint256 y, uint256 utilId) public {
@@ -110,83 +93,41 @@ contract Map is ERC2771Context, ERC721 {
         }
     }
 
-    function placeItems(
-        uint256[] memory x,
-        uint256[] memory y,
-        uint256[] memory utilId
-    ) public {
-        if (x.length != y.length) revert InvalidLength();
-        if (x.length != utilId.length) revert InvalidLength();
-        for (uint256 i = 0; i < x.length; i++) {
-            placeItem(x[i], y[i], utilId[i]);
+    function tokenURI(uint256 id) public view virtual override returns (string memory) {
+        return string(abi.encodePacked(baseUri, uint2str(id)));
+    }
+
+    // Helper function to convert uint to string
+    function uint2str(uint256 _i) internal pure returns (string memory) {
+        if (_i == 0) return "0";
+        
+        uint256 j = _i;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j /= 10;
         }
-    }
-
-    function updateItems(
-        uint256[] memory x,
-        uint256[] memory y,
-        uint256[] memory utilId
-    ) public {
-        if (x.length != y.length) revert InvalidLength();
-        if (x.length != utilId.length) revert InvalidLength();
-        for (uint256 i = 0; i < x.length; i++) {
-            updateItem(x[i], y[i], utilId[i]);
+        
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        while (_i != 0) {
+            bstr[--k] = bytes1(uint8(48 + _i % 10));
+            _i /= 10;
         }
+        
+        return string(bstr);
     }
 
-    function tokenURI(
-        uint256 id
-    ) public view virtual override returns (string memory) {
-        return string.concat(baseUri, LibString.toString(id));
-    }
-
-    function onERC1155Received(
-        address,
-        address,
-        uint256,
-        uint256,
-        bytes memory
-    ) public virtual returns (bytes4) {
-        return ERC1155TokenReceiver.onERC1155Received.selector;
-    }
-
-    function onERC1155BatchReceived(
-        address,
-        address,
-        uint256[] memory,
-        uint256[] memory,
-        bytes memory
-    ) public virtual returns (bytes4) {
-        return ERC1155TokenReceiver.onERC1155BatchReceived.selector;
-    }
-
-    function _msgSender()
-        internal
-        view
-        virtual
-        override(Context, ERC2771Context)
-        returns (address sender)
-    {
+    function _msgSender() internal view virtual override(Context, ERC2771Context) returns (address sender) {
         return ERC2771Context._msgSender();
     }
 
-    function _msgData()
-        internal
-        view
-        virtual
-        override(Context, ERC2771Context)
-        returns (bytes calldata)
-    {
+    function _msgData() internal view virtual override(Context, ERC2771Context) returns (bytes calldata) {
         return ERC2771Context._msgData();
     }
 
-    function _contextSuffixLength()
-        internal
-        view
-        virtual
-        override(Context, ERC2771Context)
-        returns (uint256)
-    {
-        return 20;
+    // Overriding _contextSuffixLength to resolve conflict with Context and ERC2771Context
+    function _contextSuffixLength() internal view virtual override(ERC2771Context, Context) returns (uint256) {
+        return 20;  // Adjust the length if needed, 20 is the default for ERC2771Context
     }
 }
